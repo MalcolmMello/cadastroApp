ff113f724cec86739866a4771e76475294895173
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-23 21:33:47 -0300

    contexto criado e estilização geral feita

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -1,18 +1,48 @@
 // Contenxt, reducer, provider, hook 
-import { createContext, useContext, useReducer } from "react";
+import { createContext, ReactNode,  useContext, useReducer } from "react";
+
+type State = {
+    currentStep: number;
+    name: string;
+    level: 0 | 1;
+    email: string;
+    github: string
+};
+
+type Action = {
+    type: FormActions;
+    payload: any;
+};
+
+type ContextType = {
+    state: State;
+    dispatch: (action: Action) => void
+}
+
+type FormProviderProps = {
+    children: ReactNode 
+}
+
+const initialData: State = {
+    currentStep: 0,
+    name: '',
+    level: 0,
+    email: '',
+    github: ''
+}
 
 // Context
-const FormContext = createContext(undefined)
+const FormContext = createContext<ContextType | undefined>(undefined)
 
 // Reducer
-enum FormActions {
+export enum FormActions {
     setCurrentStep,
     setName,
     setLevel,
     setEmail,
     setGithub
 }
-const formReducer = (state, action) => {
+const formReducer = (state: State, action: Action) => {
     switch(action.type) {
         case FormActions.setCurrentStep:
             return {...state, currentStep: action.payload}
@@ -27,4 +57,24 @@ const formReducer = (state, action) => {
         default:
             return state;
     }
+}
+
+// Provider;
+export const FormProvider = ({children}: FormProviderProps) => {
+    const [state, dispatch] = useReducer(formReducer, initialData);
+    const value = { state, dispatch}
+    return (
+        <FormContext.Provider value={value}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+// Context Hook
+export const useForm = () => {
+    const context = useContext(FormContext)
+    if(context === undefined) {
+        throw new Error('useForm precisa ser usado dentro do FormProvider')
+    }
+    return context
 }
