d35514894af57d1afc19468057416205d992f18b
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-24 08:16:40 -0300

    passo 2 completo

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,10 +1,35 @@
+import { useHistory } from 'react-router-dom'
 import * as C from './styles'
+import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
 
 export const FormStep1 = () => {
+    const history = useHistory()
+    const {state, dispatch} = useForm();
+
+    useEffect(()=>{
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        })
+    }, [])
+
     const handleNextStep = () => {
-        
+        if(state.name !== '') {
+            history.push('/step2')
+        } else {
+            alert("Preencha os dados")
+        }  
     }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+
     return (
         <Theme>
             <C.Container>
@@ -19,6 +44,8 @@ export const FormStep1 = () => {
                     <input
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
 
